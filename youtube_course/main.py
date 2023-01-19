743ed46077a51fd537c38f9ca40fa1b0e0cba0f9
nkhaldi <mnk80898@gmail.com>
2023-01-19 23:17:27 +0400

    finished course

--- youtube_course/main.py
+++ youtube_course/main.py
@@ -1,7 +1,7 @@
 from typing import List
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 
-from schemas import Book
+from schemas import Book, Author, BookOut
 
 
 app = FastAPI()
@@ -12,16 +12,38 @@ def home():
     return {"key": "hello"}
 
 
-@app.post('/book')
-def create_book(item: Book):
-    return item
-
-
 @app.get('/book')
-def get_book(q: List[str] = Query(["qwe", "asd"], description="Search book", deprecated=True)):
+def get_book(
+    q: List[str] = Query(
+        ["qwe", "asd"],
+    description="Search book", deprecated=True)
+):
     return q
 
 
 @app.get('/book/{pk}')
-def get_single_book(pk: int = Path(..., gt=1, le=20), pages: int = Query(None, gt=10, le=500)):
+def get_single_book(
+    pk: int = Path(..., gt=1, le=20),
+    pages: int = Query(None, gt=10, le=500)
+):
     return {"pk": pk, "pages": pages}
+
+
+@app.post(
+    '/book',
+    response_model=Book,
+    response_model_exclude={"pages", "date"},
+    response_model_exclude_unset=True
+)
+def create_book(item: Book, author: Author, quantity: int = Body(...)):
+    return {"item": item, "author": author, "quantity": quantity}
+
+
+@app.post('/bookout', response_model=BookOut)
+def create_bookout(item: Book):
+    return BookOut(**item.dict(), id=3)
+
+
+@app.post('/author')
+def create_author(author: Author = Body(..., embed=True)):
+    return {"author": author}
--- youtube_course/schemas.py
+++ youtube_course/schemas.py
@@ -1,10 +1,27 @@
 from typing import List
-from pydantic import BaseModel
 from datetime import date
+from pydantic import BaseModel, Field, validator
 
 
 class Genre(BaseModel):
     name: str
+
+
+class Author(BaseModel):
+    first_name: str = Field(..., max_length=25)
+    last_name: str
+    age: int = Field(
+        ..., gt=15, lt=90,
+        description="Author's age must be more than 15 and less than 90"
+    )
+
+'''
+    @validator('age')
+    def check_age(cls, v):
+        if 15 < v < 90:
+            raise ValueError("Author's age must be more than 15 and less than 90")
+        return v
+'''
 
 
 class Book(BaseModel):
@@ -12,6 +29,10 @@ class Book(BaseModel):
     writer: str
     duration: str
     date: date
-    summary: str
-    genres: List[Genre]
+    summary: str 
+    genres: List[Genre] = []
     pages: int
+
+
+class BookOut(Book):
+    id: int
