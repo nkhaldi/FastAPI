0372dc17cd1acb5f4496a6eef96ae5b9e18c8e23
nkhaldi <mnk80898@gmail.com>
2023-04-22 11:25:41 +0300

    flake8

--- youtube_artemshumeiko/main.py
+++ youtube_artemshumeiko/main.py
@@ -11,30 +11,15 @@ fake_users = [
     {'id': 3, 'role': 'trader', 'name': 'Matt'}
 ]
 
+fake_trades = [
+    {'id': 1, 'user_id': 1, 'currency': 'BTC', 'side': 'buy', 'price': 123, 'amount': 2.12},
+    {'id': 2, 'user_id': 1, 'currency': 'BTC', 'side': 'sell', 'price': 125, 'amount': 2.12},
+]
+
 
 @app.get('/users/{user_id}')
 def get_user(user_id: int):
     return [user for user in fake_users if user.get('id') == user_id]
-
-
-fake_trades = [
-    {
-        'id': 1,
-        'user_id': 1,
-        'currency': 'BTC',
-        'side': 'buy',
-        'price': 123,
-        'amount': 2.12
-    },
-    {
-        'id': 2,
-        'user_id': 1,
-        'currency': 'BTC',
-        'side': 'sell',
-        'price': 125,
-        'amount': 2.12
-    },
-]
 
 
 @app.get('/trades')
